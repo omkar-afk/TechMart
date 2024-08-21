70f493b67665d90b8ba2b14a9a5f90cc82ae1992
Omkar Deshmukh <omkar@Omkars-MacBook-Air.local>
2024-08-21 09:05:31 +0530

    made changes for shady loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -36,21 +36,38 @@ function Home() {
       <Wrapper condition= {searchValue}selectCategory = {selectCategory} setSelectCategory ={setSelectCategory}checkboxes = {checkboxes} setCheckboxes = {setCheckboxes}>
       {items.map(item => {
         return (
-          <div className="card bg-white shadow-md outline-black text-black ">
-            
-              <img src="https://th.bing.com/th/id/OIP.p85NSH7Ra7R4fouWWuUd0wHaJo?rs=1&pid=ImgDetMain" className='aspect-[4/3] object-cover bg-black rounded-2xl mx-4 mt-4' alt={item.name} />
-
-            <div className="card-body p-4 pt-3">
-              <h2 className="card-title font-semibold text-2xl">Rs{item.price}</h2>
-              <div className='font-medium'>{item.name} | {item.description}</div>
-              <div className="card-actions justify-end items-end">
-                <button className="btn btn-primary border-0 bg-black text-white  hover:bg-gray-800 ">Buy Now</button>
-              </div>
-            </div>
-          </div>
+          <div class="card bg-white shadow-md outline-black text-black">
+  <div class="skeleton h-48 w-full rounded-2xl bg-gray-200"></div>
+  <div class="card-body p-4 pt-3">
+    <div class="skeleton h-8 w-32 bg-gray-200"></div>
+    <div class="skeleton h-4 w-full bg-gray-200 mt-2"></div>
+    <div class="skeleton h-4 w-full bg-gray-200 mt-2"></div>
+    <div class="skeleton h-4 w-full bg-gray-200 mt-2"></div>
+    <div class="flex justify-end items-end mt-4">
+      <div class="skeleton h-8 w-20 bg-gray-200"></div>
+    </div>
+  </div>
+</div>
         )
       })}
       </Wrapper>
+      <Wrapper condition= {searchValue}selectCategory = {selectCategory} setSelectCategory ={setSelectCategory}checkboxes = {checkboxes} setCheckboxes = {setCheckboxes}>
+      {items.map(item => {
+        return (
+          <div className="card bg-white shadow-md outline-black text-black ">
+<img src="https://th.bing.com/th/id/OIP.p85NSH7Ra7R4fouWWuUd0wHaJo?rs=1&pid=ImgDetMain" className='aspect-[4/3] object-cover bg-black rounded-2xl mx-4 mt-4' alt={item.name} />
+<div className="card-body p-4 pt-3">
+<h2 className="card-title font-semibold text-2xl">Rs{item.price}</h2>
+<div className='font-medium'>{item.name} | {item.description}</div>
+<div className="card-actions justify-end items-end">
+<button className="btn btn-primary border-0 bg-black text-white hover:bg-gray-800 ">Buy Now</button>
+</div>
+</div>
+</div>
+        )
+      })}
+      </Wrapper>
+
       </div>
 
       // </div>
